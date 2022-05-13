cd1fc38da5ce8228fdc34ecab40cb33eed07d609
alisherabdullaxatov <muratjiyankulov@gmail.com>
2022-05-13 11:22:00 +0500

    html css js

--- js/main.js
+++ js/main.js
@@ -16,39 +16,57 @@
 
 //     });
 // });
-let tab;
-let text;
+// let tab;
+// let text;
 
 
-window.onload = function(){
-    tab = document.querySelectorAll(".tab");
-    text = document.querySelectorAll(".text");
-    tabsHide()
-}
+// window.onload = function(){
+//     tab = document.querySelectorAll(".tab");
+//     text = document.querySelectorAll(".text");
+//     tabsHide()
+// }
 
 
-function tabsHide(){
-    for(let i=0; i<text.length; i++){
-        tab[i].classList.remove("active");
-        text[i].classList.add("hide")
-        text[i].classList.remove("show")
+// function tabsHide(){
+//     for(let i=0; i<text.length; i++){
+//         tab[i].classList.remove("active");
+//         text[i].classList.add("hide")
+//         text[i].classList.remove("show")
 
-    }
+//     }
 
-}
+// }
 
 
-document.getElementById("tabs").onclick = function(e){
-    let tar = e.target;
+// document.getElementById("tabs").onclick = function(e){
+//     let tar = e.target;
 
-   //  console.log(tar);
+//    //  console.log(tar);
 
-      for(let i=0; i<tab.length; i++){
-               if(tab[i] == tar){
-                tabsHide();
-                   text[i].classList.add("show");
-                   text[i].classList.remove("hide");
-                   tab[i].classList.add("active");
-               }
-      }
-}
+//       for(let i=0; i<tab.length; i++){
+//                if(tab[i] == tar){
+//                 tabsHide();
+//                    text[i].classList.add("show");
+//                    text[i].classList.remove("hide");
+//                    tab[i].classList.add("active");
+//                }
+//       }
+// }
+let tabs = document.querySelectorAll(".tab");
+let text = document.querySelectorAll(".text");
+
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () =>{
+          text.forEach((texts) => {
+          texts.classList.remove("active");
+          });
+          tabs.forEach((tab) => {
+              tab.classList.remove("active");
+          });
+          text[index].classList.add("active");
+          tabs[index].classList.add("active");
+
+
+    });
+});
